27014d26a52974884588650f6d2a65d772bd23d8
Marko Tadic <tadija@me.com>
2018-01-05 01:53:11 +0100

    Updated license

--- Example/AELogDemo watchOS Extension/ExtensionDelegate.swift
+++ Example/AELogDemo watchOS Extension/ExtensionDelegate.swift
@@ -1,10 +1,8 @@
-//
-//  ExtensionDelegate.swift
-//  AELogDemo watchOS Extension
-//
-//  Created by Marko Tadic on 4/1/16.
-//  Copyright © 2016 AE. All rights reserved.
-//
+/**
+ *  https://github.com/tadija/AELog
+ *  Copyright (c) Marko Tadić 2016-2018
+ *  Licensed under the MIT license. See LICENSE file.
+ */
 
 import WatchKit
 import AELog
