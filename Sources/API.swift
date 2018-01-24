2166370305d0b9b1a6b988b007e92a6cdd16ed7a
Marko Tadic <tadija@me.com>
2018-01-24 22:46:27 +0100

    Implementing new (better) API [in progress]

--- Sources/API.swift
+++ Sources/API.swift
@@ -8,6 +8,35 @@ import Foundation
 
 // MARK: - API / Main
 
+public func aelog(_ items: Any..., mode: Log.Mode = .print,
+                  path: String = #file, lineNumber: Int = #line, function: String = #function)
+{
+    Log.shared.write(mode: mode,
+                     thread: Thread.current,
+                     path: path,
+                     lineNumber: lineNumber,
+                     function: function,
+                     text: text(with: items))
+}
+
+private func text(with items: Any...) -> String {
+    let array = items.first.unsafelyUnwrapped as! [Any]
+    switch array.count {
+    case 0:
+        return String()
+    case 1:
+        return "\(array.first.unsafelyUnwrapped)"
+    default:
+        var text = "\n\n"
+        for (index, element) in array.enumerated() {
+            let type = Mirror(reflecting: element).subjectType
+            let description = String(reflecting: element)
+            text += "#\(index): \(type) | \(description)\n"
+        }
+        return text
+    }
+}
+
 /// Writes the textual representation of input parameters into the standard debugger output.
 ///
 /// - Note: This method is using `print` internally, but if logging is not enabled in settings it will do nothing.
